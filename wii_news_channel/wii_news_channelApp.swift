c6d70f8b85591640283488eacd36243e4e91ba31
Tom Rives <blitz@blitzlab.ninja>
2025-12-06 00:08:52 +0100

    Initial Commit

---
+++ wii_news_channel/wii_news_channelApp.swift
@@ -0,0 +1,17 @@
+//
+//  wii_news_channelApp.swift
+//  wii_news_channel
+//
+//  Created by Tom on 06/12/2025.
+//
+
+import SwiftUI
+
+@main
+struct wii_news_channelApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
